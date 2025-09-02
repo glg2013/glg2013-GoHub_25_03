998ce3ebdd09a3903fd1f9271be5c1ee6c7823ef
fairy_shrimp <fengniancong@163.com>
2025-09-02 15:34:12 +0800

    æ手机 + 短信验证码登录

--- routes/api.go
+++ routes/api.go
@@ -36,6 +36,11 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("verify-codes/captcha", vcc.ShowCaptcha)
 			authGroup.POST("verify-codes/phone", vcc.SendUsingPhone)
 			authGroup.POST("verify-codes/email", vcc.SendUsingEmail)
+
+			// 登录
+			lgc := new(auth.LoginController)
+			// 使用手机号，短信验证码进行登录
+			authGroup.POST("/login/using-phone", lgc.LoginByPhone)
 		}
 	}
 
